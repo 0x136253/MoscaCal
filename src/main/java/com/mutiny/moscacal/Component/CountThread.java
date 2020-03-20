9e5b2d420650d69cfdfe11b5595e63a93c7bf674
Anonsmd <sunmengda@outlook.com>
2020-03-20 14:10:47 +0800

    初步完成 change

--- src/main/java/com/mutiny/moscacal/Component/CountThread.java
+++ src/main/java/com/mutiny/moscacal/Component/CountThread.java
@@ -1,10 +1,16 @@
 package com.mutiny.moscacal.Component;
 
+import com.mutiny.moscacal.dao.FileAnswerMapper;
 import com.mutiny.moscacal.dto.CalDefaultDataInfo;
 import com.mutiny.moscacal.dto.CalModuleInfo;
+import com.mutiny.moscacal.pojo.FileAnswer;
+import com.mutiny.moscacal.util.CSVUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: Anonsmd
@@ -14,6 +20,8 @@ import org.springframework.stereotype.Component;
 public class CountThread {
     @Value("${cal.count}")
     private int count;
+    @Autowired
+    private RunCal runCal;
 
     public Boolean canBeRun() {
         if (RunCal.getCount()<count){
@@ -26,17 +34,13 @@ public class CountThread {
         if (!canBeRun()){
             throw new Exception("The Running Module is full");
         }
-        RunCal runCal = new RunCal(calModuleInfo);
-        Thread thread = new Thread(runCal);
-        thread.start();
+        runCal.runModule(calModuleInfo);
     }
 
     public void runDefaultData(CalDefaultDataInfo calDefaultDataInfo) throws Exception{
         if (!canBeRun()){
             throw new Exception("The Running Module is full");
         }
-        RunCal runCal = new RunCal(calDefaultDataInfo);
-        Thread thread = new Thread(runCal);
-        thread.start();
+        runCal.runDefaultData(calDefaultDataInfo);
     }
 }
--- src/main/java/com/mutiny/moscacal/Component/RunCal.java
+++ src/main/java/com/mutiny/moscacal/Component/RunCal.java
@@ -3,10 +3,16 @@ package com.mutiny.moscacal.Component;
 import com.mutiny.moscacal.FHE.EncryptDecrypt;
 import com.mutiny.moscacal.FHE.Key;
 import com.mutiny.moscacal.FHE.RecursiveDescent;
+import com.mutiny.moscacal.dao.FileAnswerMapper;
 import com.mutiny.moscacal.dto.CalDefaultDataInfo;
 import com.mutiny.moscacal.dto.CalModuleInfo;
+import com.mutiny.moscacal.pojo.FileAnswer;
+import com.mutiny.moscacal.util.CSVUtils;
 import com.mutiny.moscacal.util.RecursiveDescentUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.math.BigInteger;
@@ -16,74 +22,120 @@ import java.util.*;
  * @Author: Anonsmd
  * @Date: 2020/3/17 16:21
  */
-public class RunCal implements Runnable{
+@Component
+public class RunCal{
+    private Logger logger = LoggerFactory.getLogger(RunCal.class);
+
     private static int count=0;
-    private boolean isDefault;
-    private int id;
-    private String func;
-    private Key key;
-    private Map<Character, BigInteger>[] calMap;
-    private Map<String, List<String>> InfoMap;
+    @Autowired
+    private FileAnswerMapper fileAnswerMapper;
+    @Autowired
+    private FilePath filePath;
 
     public static int getCount() {
         return count;
     }
 
-    public Boolean stringValHandle(Character k,List<String> val){
-        int length = val.size();
+    @Async
+    public boolean runDefaultData(CalDefaultDataInfo calDefaultDataInfo){
+        Map<Character, BigInteger>[] calMap = new HashMap[calDefaultDataInfo.getSize()];
+        Map<String, List<String>> InfoMap = new HashMap<>();
+        Boolean sDefault = true;
+        int id = calDefaultDataInfo.getModuleId();
+        Key key = calDefaultDataInfo.getKey();
+        String func = calDefaultDataInfo.getFunc();
+        Map<String,List<String>> map = calDefaultDataInfo.getMap();
+        Set<String> keySet = map.keySet();
+        Iterator<String> itKeySet = keySet.iterator();
+        while (itKeySet.hasNext()){
+            String k = itKeySet.next();
+            if (k.length()==1){
+                List<String> val = map.get(k);
+                int length = val.size();
+                for (int i=0;i<length;i++){
+                    if (calMap[i]==null){
+                        calMap[i]= new HashMap<>();
+                    }
+                    calMap[i].put(k.charAt(0),new BigInteger(val.get(i)));
+                }
+            }else {
+                InfoMap.put(k,map.get(k));
+            }
+        }
+        count++;
         try {
-            for (int i=0;i<length;i++){
-                if (calMap[i]==null){
-                    calMap[i]= new HashMap<>();
-                }
-                calMap[i].put(k,new BigInteger(val.get(i)));
-            }
-        }catch (Exception e){
-            return false;
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        List<BigInteger> answer = new ArrayList<>();
+        for (int i =0;i<calMap.length;i++){
+            RecursiveDescentUtils recursiveDescentUtils = new RecursiveDescentUtils(func,decode(calMap[i],key));
+            RecursiveDescent recursiveDescent = new RecursiveDescent(func,calMap[i],key);
+            answer.add(EncryptDecrypt.decryption(recursiveDescent.exp(),key));
+        }
+        System.out.println("Run Success");
+        String path = CSVUtils.writeCsvFile(filePath+"Answ//",Sysnis(InfoMap,answer));
+        FileAnswer fileAnswer = new FileAnswer();
+        count--;
         return true;
     }
 
-
-    public boolean mapHandle(Map<String,List<String>> map){
-        try{
-            Set<String> keySet = map.keySet();
-            Iterator<String> itKeySet = keySet.iterator();
-            while (itKeySet.hasNext()){
-                String k = itKeySet.next();
-                if (k.length()==1){
-                    stringValHandle(k.charAt(0),map.get(k));
-                }else {
-                    InfoMap.put(k,map.get(k));
+    @Async
+    public boolean runModule(CalModuleInfo calModuleInfo){
+        Map<Character, BigInteger>[] calMap = new HashMap[calModuleInfo.getSize()];
+        Map<String, List<String>> InfoMap = new HashMap<>();
+        Boolean sDefault = false;
+        int id = calModuleInfo.getModuleId();
+        Key key = calModuleInfo.getKey();
+        String func = calModuleInfo.getFunc();
+        Map<String,List<String>> map = calModuleInfo.getMap();
+        Set<String> keySet = map.keySet();
+        Iterator<String> itKeySet = keySet.iterator();
+        while (itKeySet.hasNext()){
+            String k = itKeySet.next();
+            if (k.length()==1){
+                List<String> val = map.get(k);
+                int length = val.size();
+                for (int i=0;i<length;i++){
+                    if (calMap[i]==null){
+                        calMap[i]= new HashMap<>();
+                    }
+                    calMap[i].put(k.charAt(0),new BigInteger(val.get(i)));
                 }
+            }else {
+                InfoMap.put(k,map.get(k));
             }
-        }catch (Exception e){
-            return false;
         }
+        count++;
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<BigInteger> answer = new ArrayList<>();
+        for (int i =0;i<calMap.length;i++){
+            RecursiveDescentUtils recursiveDescentUtils = new RecursiveDescentUtils(func,decode(calMap[i],key));
+            RecursiveDescent recursiveDescent = new RecursiveDescent(func,calMap[i],key);
+            answer.add(EncryptDecrypt.decryption(recursiveDescent.exp(),key));
+        }
+        System.out.println("Run Success");
+        String path = CSVUtils.writeCsvFile(filePath.returnBasicPath()+"Answ//",Sysnis(InfoMap,answer));
+        FileAnswer fileAnswer = new FileAnswer();
+        count--;
         return true;
     }
 
-    public RunCal(CalDefaultDataInfo calDefaultDataInfo) {
-        this.calMap = new HashMap[calDefaultDataInfo.getSize()];
-        this.InfoMap = new HashMap<>();
-        this.isDefault = true;
-        this.id = calDefaultDataInfo.getModuleId();
-        this.key = calDefaultDataInfo.getKey();
-        this.func = calDefaultDataInfo.getFunc();
-        mapHandle(calDefaultDataInfo.getMap());
+    public Map<String,List<String> > Sysnis(Map<String, List<String>> infoMap,List<BigInteger> answer){
+        List<String>  moduleansw = new ArrayList<>();
+        for(BigInteger record : answer){
+            moduleansw.add(record.toString());
+        }
+        infoMap.put("answ",moduleansw);
+        return infoMap;
     }
 
-    public RunCal(CalModuleInfo calModuleInfo) {
-        this.calMap = new HashMap[calModuleInfo.getSize()];
-        this.InfoMap = new HashMap<>();
-        this.isDefault = true;
-        this.id = calModuleInfo.getModuleId();
-        this.key = calModuleInfo.getKey();
-        this.func = calModuleInfo.getFunc();
-        mapHandle(calModuleInfo.getMap());
-    }
-
-    public Map<Character,BigInteger> decode(Map<Character, BigInteger> map){
+    public Map<Character,BigInteger> decode(Map<Character, BigInteger> map,Key key){
         Iterator<Character> keySet = map.keySet().iterator();
         Map<Character, BigInteger> answ = new HashMap<>();
         while (keySet.hasNext()){
@@ -92,22 +144,42 @@ public class RunCal implements Runnable{
         }
         return answ;
     }
-    @Override
-    public void run() {
+
+    public List<BigInteger> Run(Map<Character, BigInteger>[] calMap,
+                    Map<String, List<String>> InfoMap,
+                    Map<String,List<String>> map,
+                    Key key,
+                    String func){
+        Set<String> keySet = map.keySet();
+        Iterator<String> itKeySet = keySet.iterator();
+        while (itKeySet.hasNext()){
+            String k = itKeySet.next();
+            if (k.length()==1){
+                List<String> val = map.get(k);
+                int length = val.size();
+                for (int i=0;i<length;i++){
+                    if (calMap[i]==null){
+                        calMap[i]= new HashMap<>();
+                    }
+                    calMap[i].put(k.charAt(0),new BigInteger(val.get(i)));
+                }
+            }else {
+                InfoMap.put(k,map.get(k));
+            }
+        }
         count++;
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
+        List<BigInteger> answer = new ArrayList<>();
         for (int i =0;i<calMap.length;i++){
-            System.out.println("num "+i+": ");
-            RecursiveDescentUtils recursiveDescentUtils = new RecursiveDescentUtils(func,decode(calMap[i]));
-            System.out.println(recursiveDescentUtils.exp());
+            RecursiveDescentUtils recursiveDescentUtils = new RecursiveDescentUtils(func,decode(calMap[i],key));
             RecursiveDescent recursiveDescent = new RecursiveDescent(func,calMap[i],key);
-            System.out.println(EncryptDecrypt.decryption(recursiveDescent.exp(),key));
+            answer.add(EncryptDecrypt.decryption(recursiveDescent.exp(),key));
         }
-        count--;
+        System.out.println("Run Success");
+        return answer;
     }
 }
